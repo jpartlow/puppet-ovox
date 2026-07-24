b22a49e21c153e8b5b6777d5971265c1dedbb1d0
Josh Partlow <jpartlow@glatisant.org>
2026-07-23 17:37:29 -0700

    Add a function to validate a TargetMap's architecture
    And return an array of any errors found. Looks for structural problems
    in the given host mappings, and for overlapping roles for roles that are
    not composable (such as compiler or complier_lb, compared with
    primary/ovdb/postgres...)
    
    Signed-off-by: Josh Partlow <jpartlow@glatisant.org>

---
+++ functions/validate_architecture.pp
@@ -0,0 +1,104 @@
+# Validate the given cluster TargetMap for architectural and role
+# errors and return an array of any errors found.
+#
+# There are constraints to what constitutes a valid cluster based on
+# the plan being able to successful install, configure and test the
+# cluster by running the agent on all nodes.
+#
+# * There must be a primary openvox-server defined.
+# * If openvoxdb nodes are defined, there most be a postgres node
+# defined or a host reference to an external postgres.
+# * Likewise, if there is a postgres reference, there must be
+# openvoxdb nodes defined.
+# * If there are compilers, then there must be a compiler
+# load-balancer node, or a $compiler_pool_address reference.
+# * And if there is more than one openvoxdb node, there likewise must
+# be an openvoxdb load-balancer node or a $ovdb_pool_address
+# reference.
+# * Overlapping compiler or load-balancer roles with another primary
+# role (primary, ovdb, postgres...).
+#
+# TODO: Warnings, for grey areas like splitting ovdb nodes across the
+# primary and additional nodes. Odd configurations like a primary with
+# openvox-server, postgres and a separate ovdb node... Things that
+# should technically work but are probably a bad idea or an
+# unnecessary complication...
+#
+# @param target_map Ovox::TargetMap instance for the cluster.
+function ovox::validate_architecture(
+  Ovox::TargetMap $target_map,
+) >> Array[String[1]] {
+
+  $arch_errs = [
+    # The TargetMap type currently requires a single Target for
+    # 'server_targets' so this error shouldn't be possible.
+    [
+      $target_map['server_targets'].empty(),
+      'No defined primary openvox-server targets.',
+    ],
+    [
+      (ovox::has_postgres($target_map) and
+        $target_map['ovdb_targets'].empty()),
+      'Postgres nodes defined, but no openvoxdb nodes in cluster.'
+    ],
+    [
+      (!$target_map['ovdb_targets'].empty() and
+        !ovox::has_postgres($target_map)) ,
+      @(EOS/L),
+        Openvoxdb nodes defined, but no Postgres nodes defined \
+        or referenced, managed or unmanaged.
+        |- EOS
+    ],
+    [
+      (!$target_map['compiler_targets'].empty() and
+        (ovox::get_pool_address('compiler', $target_map) =~ Undef)),
+      @(EOS/L),
+        Compilers defined, but no compiler load-balancer nodes \
+        or compiler_pool_address is set.
+        |- EOS
+    ],
+    [
+      (($target_map['ovdb_targets'].count() > 1) and
+        (ovox::get_pool_address('ovdb', $target_map) =~ Undef)),
+      @(EOS/L),
+        Multiple Openvoxdb nodes defined, but no openvoxdb \
+        load-balancer nodes or ovdb_pool_address is set.
+        |- EOS
+    ],
+    # This shouldn't be possible based on how generate_target_map()
+    # works, but a manually constructed TargetMap could still
+    # violate this constraint...
+    [
+      (!$target_map['postgres_targets'].empty() and
+       !$target_map['unmanaged_postgres_hosts'].empty()),
+      'Both internal and external PostgreSQL has been defined.',
+    ],
+  ].reduce([]) |$errors, $i| {
+    $test = $i[0]
+    $errmsg = $i[1]
+    $test ? {
+      true    => $errors + [$errmsg],
+      default => $errors,
+    }
+  }
+
+  # Maps the OpenVox ov_role classes to the distinct set of targets
+  # they will be classifying in the configuration phase.
+  $role_map = ovox::derive_role_map($target_map)
+
+  # Check for invalid role intersections.
+  $role_intersection_errors = [
+    'compiler',
+    'compiler_lb',
+    'ovdb_lb',
+  ].reduce([]) |$errors, $role| {
+    $intersection_errors =
+      ovox::check_role_target_intersection($role, $role_map)
+    $intersection_errors.empty() ? {
+      false   => $errors + $intersection_errors,
+      default => $errors,
+    }
+  }
+
+  $arch_errs + $role_intersection_errors
+}
---
+++ spec/functions/validate_architecture_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+
+describe 'ovox::validate_architecture' do
+  include BoltSpec::BoltContext
+
+  around(:each) do |example|
+    in_bolt_context { example.run }
+  end
+
+  include_context('shared target maps')
+
+  it 'returns an empty error for set arches without overlapping roles' do
+    is_expected.to run.with_params(t_target_map).and_return([])
+    is_expected.to run.with_params(s_target_map).and_return([])
+    is_expected.to run.with_params(m_target_map).and_return([])
+    is_expected.to run.with_params(l_target_map).and_return([])
+    is_expected.to run.with_params(h_target_map).and_return([])
+  end
+
+  context 'custom arch' do
+    it 'raises an error if ovdb present and postgres is missing' do
+      s_target_map['postgres_targets'] = []
+      $errs = call_function('ovox::validate_architecture', s_target_map)
+      expect($errs).to match([%r{Openvoxdb nodes defined, but no Postgres}])
+    end
+
+    it 'raises an error if postgres is present and ovdb is missing' do
+      s_target_map['ovdb_targets'] = []
+      $errs = call_function('ovox::validate_architecture', s_target_map)
+      expect($errs).to match([%r{Postgres nodes defined, but no openvoxdb}])
+      $errs = call_function('ovox::validate_architecture', unmanaged_postgres(s_target_map))
+      expect($errs).to match([%r{Postgres nodes defined, but no openvoxdb}])
+    end
+
+    it 'raises an error if compilers are present and no lb or pool address' do
+      m_target_map['compiler_lb_targets'] = []
+      $errs = call_function('ovox::validate_architecture', m_target_map)
+      expect($errs).to match([%r{Compilers defined, but no.*load-balancer}])
+      m_target_map['compiler_pool_address'] = 'foo.lb.spec'
+      is_expected.to run.with_params(m_target_map).and_return([])
+    end
+
+    it 'raises an error if multiple ovdbs are present and no lb or pool address' do
+      h_target_map['ovdb_lb_targets'] = []
+      $errs = call_function('ovox::validate_architecture', h_target_map)
+      expect($errs).to match([%r{Multiple Openvoxdb nodes defined, but no.*load-balancer}])
+
+      h_target_map['ovdb_pool_address'] = 'foo.lb.spec'
+      is_expected.to run.with_params(h_target_map).and_return([])
+    end
+
+    it 'raises an error if postgres_targets and unmanaged_postgres_hosts defined' do
+      s_target_map['unmanaged_postgres_hosts'] = ['some.postgres.spec']
+      $errs = call_function('ovox::validate_architecture', s_target_map)
+      expect($errs).to match([%r{Both internal and external PostgreSQL}])
+    end
+  end
+
+  context 'overlapping roles' do
+    it 'raises an error if compilers overlap another role' do
+      m_target_map['compiler_targets'] << m_target_map['server_targets'][0]
+
+      $errs = call_function('ovox::validate_architecture', m_target_map)
+      expect($errs).to match([%r{compiler hostnames should be unique}])
+    end
+
+    it 'raises an error if compiler_lbs overlap another role' do
+      h_target_map['ovdb_targets'] << h_target_map['compiler_lb_targets'][0]
+
+      $errs = call_function('ovox::validate_architecture', h_target_map)
+      expect($errs).to match([%r{compiler_lb hostnames should be unique}])
+    end
+
+    it 'raises an error if ovdb_lbs overlap another role' do
+      h_target_map['compiler_lb_targets'] = h_target_map['ovdb_lb_targets']
+
+      $errs = call_function('ovox::validate_architecture', h_target_map)
+      expect($errs).to match(
+        [
+          %r{compiler_lb hostnames should be unique},
+          %r{ovdb_lb hostnames should be unique},
+        ]
+      )
+    end
+  end
+end
