565e273621fa74fe23fbc2791720886d03a47047
Josh Partlow <jpartlow@glatisant.org>
2026-07-31 14:41:56 -0700

    Stub a run_agent task to be used by the configure plan
    Needs to be fleshed out to actually run the agent, but allows for spec
    testing of configure subplan for now.
    
    Signed-off-by: Josh Partlow <jpartlow@glatisant.org>

---
+++ tasks/run_agent.rb
@@ -0,0 +1,11 @@
+#! /opt/puppetlabs/puppet/bin/ruby
+# frozen_string_literal: true
+
+class RunAgent
+  def self.run
+    puts 'implement'
+    exit 1
+  end
+end
+
+RunAgent.run if __FILE__ == $PROGRAM_NAME
